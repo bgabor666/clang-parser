cceb46471864aa0611bbad33915ef33356266c9c
Gabor Ballabas <gaborb@inf.u-szeged.hu>
2018-05-24 13:21:51 +0000

    Add python bindings sandbox

---
+++ python-bindings/test.h
@@ -0,0 +1,17 @@
+//#include <stdio.h>
+//#include <time.h>
+
+typedef long test_int;
+
+/*
+struct TestStruct {
+  int a;
+  char b;
+};
+*/
+
+int func(int a, char b);
+
+int func4(int a, test_int test_b, int c, char d);
+
+//int func5(int a, time_t b, int c, char d);
