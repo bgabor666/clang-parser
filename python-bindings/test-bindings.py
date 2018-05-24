4b7c2d17ceaed5743c7e5fa32b8484abdb10b651
Gabor Ballabas <gaborb@inf.u-szeged.hu>
2018-05-24 15:19:16 +0000

    Add visitor for function parameters

--- python-bindings/test-bindings.py
+++ python-bindings/test-bindings.py
@@ -1,30 +1,38 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+
 import pprint
 import sys
 import clang.cindex
 
+def parm_visitor(cursor):
+  if cursor.kind == clang.cindex.CursorKind.PARM_DECL:
+    #print('>>> Found %s type %s [line=%s, col=%s]' % (
+            #cursor.displayname, cursor.type.get_canonical().spelling, cursor.location.line, cursor.location.column))
+    print('{} {}'.format(cursor.type.get_canonical().spelling, cursor.displayname), end='')
+
 def visitor(cursor):
-  print 'visiting', cursor.kind
-  #if cursor.kind == clang.cindex.CursorKind.CALL_EXPR:
-    #print 'Found %s [line=%s, col=%s]' % (
-            #cursor.displayname, cursor.location.line, cursor.location.column)
 
   if cursor.kind == clang.cindex.CursorKind.FUNCTION_DECL:
-    #print 'Found %s return type %s [line=%s, col=%s]' % (
-            #cursor.canonical.displayname, cursor.type.get_result().get_canonical().kind, cursor.location.line, cursor.location.column)
+    print('//{} {}'.format(cursor.type.get_result().spelling, cursor.canonical.displayname))
+    #print '//{} {}'.format(cursor.type.get_result().spelling, cursor.spelling)
 
-    #print '{} {}'.format(cursor.type.get_result().get_canonical().kind, cursor.canonical.displayname)
-    print '{} {}'.format(cursor.type.get_result().get_canonical().spelling, cursor.canonical.displayname)
+    print('Built-in displayname: {} {}'.format(cursor.type.get_result().get_canonical().spelling, cursor.canonical.displayname))
 
-  if cursor.kind == clang.cindex.CursorKind.PARM_DECL:
-    #print 'Found %s type %s [line=%s, col=%s]' % (
-            #cursor.displayname, cursor.type.get_canonical().kind, cursor.location.line, cursor.location.column)
-    print 'Found %s type %s [line=%s, col=%s]' % (
-            cursor.displayname, cursor.type.get_canonical().spelling, cursor.location.line, cursor.location.column)
+    print('Generated: {} {}('.format(cursor.type.get_result().get_canonical().spelling, cursor.spelling), end='')
+
+    function_children = list(cursor.get_children())
+    number_of_children = len(function_children)
+    for index in range(number_of_children):
+      if function_children[index].kind == clang.cindex.CursorKind.PARM_DECL:
+        parm_visitor(function_children[index])
+        if index < number_of_children - 1:
+          print(', ', end='')
+
+    print(')')
 
   children = list(cursor.get_children())
-  print '  has {0} children'.format(len(children))
   for child in children:
     visitor(child)
 
@@ -34,7 +42,7 @@ tu = index.parse(sys.argv[1], args=['-x', 'c++'])
 
 diagnostics = list(tu.diagnostics)
 if len(diagnostics) > 0:
-    print 'There were parse errors'
+    print('There were parse errors')
     pprint.pprint(diagnostics)
 else:
     visitor(tu.cursor)
--- python-bindings/test.h
+++ python-bindings/test.h
@@ -12,6 +12,8 @@ struct TestStruct {
 
 int func(int a, char b);
 
+int func2(int a, char b, int c);
+
 test_int func4(int a, test_int test_b, int c, char d);
 
 //int func5(int a, time_t b, int c, char d);
